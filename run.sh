94b30df8955eec71997eff7d2fa89c663e1a57ae
pistachiyoda <fujisawamai21@gmail.com>
2020-12-12 19:33:18 +0900

    正解と比べてdiffがある場合、ない場合を（少し）わかりやすく出力するようにした

--- run.sh
+++ run.sh
@@ -2,12 +2,12 @@
 make fclean
 tests="./tests/*"
 YELLOW='\033[0;33m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
 rm -r my_result
 mkdir my_result
-
-#test
 
 function echo_myresult(){
     echo ""
@@ -17,9 +17,21 @@ function echo_myresult(){
     ./a.out $2
 }
 
+function echo_compare_result(){
+    diff -u my_result/BUFFER_SIZE_$1x$2 correct_result/BUFFER_SIZE_$1x$2
+}
+
 function output_myresult(){
     file_name=`echo $2 | sed "s/\.\/tests\///g"`
     ./a.out $2 > my_result/BUFFER_SIZE_$1x$file_name
+    echo -e "${YELLOW}=====BUFFER_SIZE=$1 x $2=====${NC}" 
+    diff my_result/BUFFER_SIZE_$1x$file_name correct_result/BUFFER_SIZE_$1x$file_name > /dev/null 2>&1
+    if [ $? -eq 1 ] ; then
+        echo -e "${RED}=================THERE ARE DIFFERENCES=================${NC}"
+        echo_compare_result $1 $file_name
+    else
+        echo -e "${GREEN}=================THERE IS NO DIFFERENCES=================${NC}"
+    fi
 }
 
 function output_result(){
@@ -30,7 +42,6 @@ function output_result(){
             continue
         fi
         output_myresult $1 $file
-        echo_myresult $1 $file
     done
 }
 
@@ -45,12 +56,9 @@ output_result $small_buf4
 
 same_buf=100
 output_myresult $same_buf ./tests/same_buf_100
-echo_myresult $same_buf ./tests/same_buf_100
 
 plus1_buf=101
 output_myresult $plus1_buf ./tests/same_buf_100
-echo_myresult $plus1_buf ./tests/same_buf_100
 
 minus1_buf=99
 output_myresult $minus1_buf ./tests/same_buf_100
-echo_myresult $minus1_buf ./tests/same_buf_100
