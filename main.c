fbaf79ec867bb192c68c6d96ca030cd7aba9a7f3
pistachiyoda <fujisawamai21@gmail.com>
2020-12-12 16:59:28 +0900

    差分チェック用の正しいアウトプット(correct_result)と、自分のコードのアウトプット(my_result)を作成するスクリプトを作成。printf系は一旦コメントアウト

--- main.c
+++ main.c
@@ -1,39 +1,18 @@
-#include "./cpy_gnl/get_next_line.h"
+#include "./correct_get_next_line/get_next_line.h"
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 int main(int argc, char **argv)
 {
     int fd;
     char *line;
-    printf("\x1b[34m");
-    printf("\x1b[1m");
-    printf("********************Summary********************\n");
-    printf("\x1b[37m");
-    printf("\x1b[0m");
-    printf("argc:%d\n", argc);
-    printf("argv[1]:%s\n", argv[1]);
-    fd = open(argv[1], O_RDONLY);
-    printf("\x1b[34m");
-    printf("======OUTPUT(line)=====\n");
-    printf("\x1b[39m");
-    while (1)
-    {
-        int ret = get_next_line(fd, &line);
-        printf("%s\n",line);
-        if (ret <= 0)
-            break ;
-    }
-    printf("\x1b[34m");
-    printf("\x1b[1m");
-    printf("********************detail********************\n");
-    printf("\x1b[37m");
-    printf("\x1b[0m");
     fd = open(argv[1], O_RDONLY);
     while (1)
     {
         int ret = get_next_line(fd, &line);
-        printf("\x1b[34m");
         printf("======OUTPUT(line)=====\n");
-        printf("\x1b[39m");
         printf("ret:%d\n",ret);
         printf("%s\n",line);
         if (ret <= 0)
@@ -41,3 +20,43 @@ int main(int argc, char **argv)
     }
     exit(1);
 }
+// {
+//     int fd;
+//     char *line;
+//     printf("\x1b[34m");
+//     printf("\x1b[1m");
+//     printf("********************Summary********************\n");
+//     printf("\x1b[37m");
+//     printf("\x1b[0m");
+//     printf("argc:%d\n", argc);
+//     printf("argv[1]:%s\n", argv[1]);
+//     fd = open(argv[1], O_RDONLY);
+//     printf("\x1b[34m");
+//     printf("======OUTPUT(line)=====\n");
+//     printf("\x1b[39m");
+//     while (1)
+//     {
+//         int ret = get_next_line(fd, &line);
+//         printf("%s\n",line);
+//         if (ret <= 0)
+//             break ;
+//     }
+//     printf("\x1b[34m");
+//     printf("\x1b[1m");
+//     printf("********************detail********************\n");
+//     printf("\x1b[37m");
+//     printf("\x1b[0m");
+//     fd = open(argv[1], O_RDONLY);
+//     while (1)
+//     {
+//         int ret = get_next_line(fd, &line);
+//         printf("\x1b[34m");
+//         printf("======OUTPUT(line)=====\n");
+//         printf("\x1b[39m");
+//         printf("ret:%d\n",ret);
+//         printf("%s\n",line);
+//         if (ret <= 0)
+//             break ;
+//     }
+//     exit(1);
+// }
--- run.sh
+++ run.sh
@@ -1,21 +1,28 @@
 #!/bun/bash
 make fclean
-files="./tests/*"
-RED='\033[0;33m'
+tests="./tests/*"
+YELLOW='\033[0;33m'
 NC='\033[0m' # No Color
 
+#すでにディレクトリやファイルが有るときは作り直す
+rm -r my_result
+mkdir my_result
+
 function echo_result(){
-    echo ""
-    echo ""
-    echo -e "${RED}=====BUFFER_SIZE=$1 x $2=====${NC}" | tr '\0' '$$'
-    echo ""
-    ./a.out $2
+    # echo ""
+    # echo ""
+    # echo -e "${YELLOW}=====BUFFER_SIZE=$1 x $2=====${NC}" | tr '\0' '$$'
+    # echo ""
+    file_name=`echo $2 | sed "s/\.\/tests\///g"`
+    #./a.out $2
+    ./a.out $2 > my_result/BUFFER_SIZE_$1x$file_name
 }
 
 function output_result(){
-    gcc -o ./a.out -Wall -Wextra -Werror -D BUFFER_SIZE=$1 main.c ./cpy_gnl/get_next_line.c ./cpy_gnl/get_next_line_utils.c
-    for file in $files; do
-        if [[ $file =~ ./tests/same_buf_* ]]; then
+    gcc -o ./a.out -D BUFFER_SIZE=$1 main.c ./cpy_gnl/get_next_line.c ./cpy_gnl/get_next_line_utils.c
+    #gcc -o ./a.out -D BUFFER_SIZE=$1 main.c ./correct_get_next_line/get_next_line.c ./correct_get_next_line/libft/libft.a
+    for file in $tests; do
+        if [[ $file =~ ./tests/same_buf_100 ]]; then
             continue
         fi
         echo_result $1 $file
@@ -35,7 +42,7 @@ same_buf=100
 echo_result $same_buf ./tests/same_buf_100
 
 plus1_buf=101
-echo_result $plus1_buf ./tests/same_buf_101
+echo_result $plus1_buf ./tests/same_buf_100
 
 minus1_buf=99
-echo_result $minus1_buf ./tests/same_buf_99
+echo_result $minus1_buf ./tests/same_buf_100
